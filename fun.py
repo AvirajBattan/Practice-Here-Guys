b2ce06b8600f625ae5957d93fcea2c35e5163718
Aviraj Battan <battanaviraj@gmail.com>
2023-03-03 14:13:23 +0530

    fun file change made in line 5

--- fun.py
+++ fun.py
@@ -2,6 +2,7 @@ def solve(a,b):
     try:
         if isinstance(a,str) or isinstance(b,str):
             return "Enter integer"
+        print("Line added in 5")
         return a*b*10
     
     except :
